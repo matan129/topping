0dfbad7e526c69da825222d6e5c425df6cca04a6
liad <liad.abenzour64@gmail.com>
2017-02-23 22:42:08 +0200

    split constraints

--- solve.py
+++ solve.py
@@ -3,17 +3,15 @@ from ortools.linear_solver import pywraplp as ps
 from main import *
 
 
+
 def output(name, data):
     with open(os.path.join('res', name + '.out')) as f:
         f.write(data)
 
-
-def solve(name):
-    data = Data(name)
-    solver = ps.Solver('SHA7', ps.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
+def solve_with_subset(data, videos, final_caches):
     y = {}
     t = {}
-    for v in data.videos:
+    for v in videos:
         for c in v.caches.keys():
             y[(v, c)] = solver.IntVar(0.0, 1.0, 'y_{0}_{1}'.format(str(v.id), str(c)))
 
@@ -27,10 +25,10 @@ def solve(name):
 
     constraints = {}
 
-    for v in data.videos:
+    for v in videos:
         for c in v.caches.keys():
             if c not in constraints.keys():
-                constraints[c] = solver.Constraint(0.0, data.max_cache_capacity)
+                constraints[c] = solver.Constraint(0.0, data.caches[c])
             constraints[c].SetCoefficient(y[(v, c)], v.size + 0.0)
 
         for e in v.endpoints:
@@ -51,25 +49,32 @@ def solve(name):
         N += r.num_requests
 
     for r in data.requests:
-        v = data.videos[r.video_id]
+        v = videos[r.video_id]
         e = data.endpoints[r.endpoint_id]
         for c in e.caches.keys():
             objective.SetCoefficient(t[(v, c, e)], (r.num_requests * e.caches[c]) / N)
         objective.SetCoefficient(t[(v, e)], (r.num_requests * e.latency_to_data_center) / N)
 
     objective.SetMinimization()
-    print solver.Solve() == ps.Solver.OPTIMAL
 
-    from collections import defaultdict
-    final_caches = defaultdict(list)
     for (v, c), value in y.iteritems():
         if value.solution_value() == 1:
             final_caches[c].append(v.id)
+            data.caches[c] = data.caches[c] - v.size
+
+
+
+def solve(name):
+    from collections import defaultdict
+    data = Data(name)
+    solver = ps.Solver('SHA7', ps.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
+    final_caches = defaultdict(list)
+    for i in xrange(0, len(data.videos) - 64, 64):
+        final_caches = solve_with_subset(data, data.videos[i:i+64], final_caches)
 
     lines = [str(len(final_caches.keys()))]
     for cache_id, videos in final_caches.iteritems():
         lines.append(' '.join(map(str, [cache_id] + videos)))
-
     output(name, '\n'.join(lines))
 
 
