901040998390ae38e9fccafb614467c1bacbe3cb
Matan Rosenberg <matan129@gmail.com>
2017-02-23 21:16:12 +0200

    Add solve

---
+++ solve.py
@@ -0,0 +1,53 @@
+from ortools.linear_solver import pywraplp as ps
+
+from main import *
+
+
+def solve():
+    data = Data('example')
+    solver = ps.Solver('SHA7', ps.Solver.GLOP_LINEAR_PROGRAMMING)
+
+    y = {}
+    t = {}
+    for v in data.videos:
+        for c in v.caches.keys():
+            y[(v, c)] = solver.IntVar(0.0, 1.0, 'y {0} {1}'.format(str(v), str(c)))
+
+        for e in v.endpoints:
+            for c in e.caches.keys():
+                t[(v, c, e)] = solver.IntVar(0.0, 1.0, 't {0} {1} {2}'.format(str(v), str(c), str(e)))
+
+    constraints = {}
+    for c in data.caches.keys():
+        constraints[c] = solver.Constraint(0.0, data.max_cache_capacity)
+
+    for v in data.videos:
+        for c in v.caches.keys():
+            constraints[c].SetCoefficient(y[(v, c)], 1)
+        
+        for e in v.endpoints:
+            constraints[(e, v)] = solver.Constraint(0.0, 1.0)
+            
+            for c in e.caches.keys():
+                constraints[(e, v)].SetCoefficient(t[(v, c, e)], 1)
+                constraints[(v, c, e)] = solver.Constraint(0.0, solver.infinity())
+                constraints[(v, c, e)].SetCoefficient(t[(v, c, e)], 1)
+                constraints[(v, c, e)].SetCoefficient(y[(v, c)], -1)
+        
+    objective = solver.Objective()
+
+    N = 0
+    for r in data.requests:
+        N += r.num_requests
+
+    for r in data.requests:
+        v = data.videos[r.video_id]
+        for c in v.caches.keys():
+            for e in v.endpoints: 
+                objective.SetCoefficient(t[(v, c, e)], (r.num_requests * e.caches[c]) / N)
+
+    objective.SetMinimization()
+    solver.solve()
+    
+if __name__ == '__main__':
+    solve()
