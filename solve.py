d8fa7574980afb98086994ad843aa274de35f814
Matan Rosenberg <matan129@gmail.com>
2017-02-24 20:09:03 +0200

    Add progress bars

--- solve.py
+++ solve.py
@@ -1,10 +1,10 @@
 from ortools.linear_solver import pywraplp as ps
-
+from tqdm import trange, tqdm
 from main import *
 
 
 def output(name, data):
-    with open(os.path.join('res', name + '.out')) as f:
+    with open(os.path.join('res', name + '.out'), 'wb') as f:
         f.write(data)
 
 
@@ -13,12 +13,12 @@ def solve(name):
     solver = ps.Solver('SHA7', ps.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
     y = {}
     t = {}
-    for v in data.videos:
-        for c in v.caches.keys():
+    for v in tqdm(data.videos, desc='video vars'):
+        for c in tqdm(v.caches.iterkeys()):
             y[(v, c)] = solver.IntVar(0.0, 1.0, 'y_{0}_{1}'.format(str(v.id), str(c)))
 
-        for e in v.endpoints:
-            for c in e.caches.keys():
+        for e in tqdm(v.endpoints, desc='video endpoints vars'):
+            for c in tqdm(e.caches.iterkeys(), desc='endpoint caches vars'):
                 if (v, c, e) not in t:
                     t[(v, c, e)] = solver.IntVar(0.0, 1.0, 't_{0}_{1}_{2}'.format(str(v.id), str(c), str(id(e))))
 
@@ -26,23 +26,26 @@ def solve(name):
                 t[(v, e)] = solver.IntVar(0.0, 1.0, 't_{0}_{1}_D'.format(str(v.id), str(id(e))))
 
     constraints = {}
+    print
+    print
 
-    for v in data.videos:
-        for c in v.caches.keys():
-            if c not in constraints.keys():
+    for v in tqdm(data.videos, desc='video constraints'):
+        for c in tqdm(v.caches.iterkeys(), desc='video caches constraints'):
+            if c not in constraints.iterkeys():
                 constraints[c] = solver.Constraint(0.0, data.max_cache_capacity)
             constraints[c].SetCoefficient(y[(v, c)], v.size + 0.0)
 
-        for e in v.endpoints:
+        for e in tqdm(v.endpoints, desc='video endpoint constraints'):
             constraints[(e, v)] = solver.Constraint(1.0, 1.0)
             constraints[(e, v)].SetCoefficient(t[(v, e)], 1)
 
-            for c in e.caches.keys():
+            for c in tqdm(e.caches.iterkeys(), desc='endpoint caches constraints'):
                 constraints[(e, v)].SetCoefficient(t[(v, c, e)], 1)
                 constraints[(v, c, e)] = solver.Constraint(-solver.infinity(), 0.0)
                 constraints[(v, c, e)].SetCoefficient(t[(v, c, e)], 1)
                 constraints[(v, c, e)].SetCoefficient(y[(v, c)], -1)
-
+    print
+    print
     print 'objective'
     objective = solver.Objective()
 
@@ -50,13 +53,16 @@ def solve(name):
     for r in data.requests:
         N += r.num_requests
 
-    for r in data.requests:
+    for r in tqdm(data.requests, desc='requests'):
         v = data.videos[r.video_id]
         e = data.endpoints[r.endpoint_id]
-        for c in e.caches.keys():
+        for c in tqdm(e.caches.iterkeys(), desc='endpoint cache'):
             objective.SetCoefficient(t[(v, c, e)], (r.num_requests * e.caches[c]) / N)
         objective.SetCoefficient(t[(v, e)], (r.num_requests * e.latency_to_data_center) / N)
 
+    print
+    print 
+    print 'solve'
     objective.SetMinimization()
     print solver.Solve() == ps.Solver.OPTIMAL
 
@@ -73,5 +79,6 @@ def solve(name):
     output(name, '\n'.join(lines))
 
 
+import sys
 if __name__ == '__main__':
-    solve('me_at_the_zoo')
+    solve(sys.argv[1])
