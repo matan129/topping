5a400facb74e52a3ce8d9473a65e758be0650c2d
Matan Rosenberg <matan129@gmail.com>
2017-02-24 19:08:43 +0200

    post

--- solve.py
+++ solve.py
@@ -8,8 +8,9 @@ def output(name, data):
     with open(os.path.join('res', name + '.out'), 'wb') as f:
         f.write(data)
 
-def solve_with_subset(data, videos, final_caches):
-    solver = ps.Solver('SHA7', ps.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
+def solve_with_subset(data, videos, final_caches, i):
+    solver = ps.Solver('SHA7' + str(i) , ps.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
+
     y = {}
     t = {}
     for v in videos:
@@ -42,7 +43,7 @@ def solve_with_subset(data, videos, final_caches):
                 constraints[(v, c, e)].SetCoefficient(t[(v, c, e)], 1)
                 constraints[(v, c, e)].SetCoefficient(y[(v, c)], -1)
 
-    print 'objective'
+    # print 'objective'
     objective = solver.Objective()
 
     N = 0
@@ -51,34 +52,40 @@ def solve_with_subset(data, videos, final_caches):
             N += r.num_requests
 
     for r in data.requests:
-        if r.video_id in videos:
-            v = videos[r.video_id]
+        if r.video_id not in videos:
+        	continue
+
+        v = videos[r.video_id]
         e = data.endpoints[r.endpoint_id]
         for c in e.caches.keys():
             objective.SetCoefficient(t[(v, c, e)], (r.num_requests * e.caches[c]) / N)
         objective.SetCoefficient(t[(v, e)], (r.num_requests * e.latency_to_data_center) / N)
 
     objective.SetMinimization()
+    solver.Solve()	
 
     for (v, c), value in y.iteritems():
         if value.solution_value() == 1:
             final_caches[c].append(v.id)
             data.caches[c] = data.caches[c] - v.size
 
+    return final_caches
 
 
 def solve(name):
     from collections import defaultdict
     data = Data(name)
     final_caches = defaultdict(list)
-    for i in xrange(0, len(data.videos) - 64, 64):
-        final_caches = solve_with_subset(data, data.videos[i:i+64], final_caches)
+    from tqdm import tqdm
+    block_size = 500
+
+    for i in tqdm(xrange(0, len(data.videos) - block_size, block_size)):
+        final_caches = solve_with_subset(data, data.videos[i:i+block_size], final_caches, i)
 
     lines = [str(len(final_caches.keys()))]
     for cache_id, videos in final_caches.iteritems():
         lines.append(' '.join(map(str, [cache_id] + videos)))
     output(name, '\n'.join(lines))
-
 
 import sys
 if __name__ == '__main__':
